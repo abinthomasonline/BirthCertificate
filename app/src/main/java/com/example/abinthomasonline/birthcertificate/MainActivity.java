fd45e68b647f2a854733c0c5b672a67373cfb730
Abin Thomas <abinthomasonline@gmail.com>
2016-03-13 23:11:39 +0530

    District Spinner Created

--- app/src/main/java/com/example/abinthomasonline/birthcertificate/MainActivity.java
+++ app/src/main/java/com/example/abinthomasonline/birthcertificate/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.abinthomasonline.birthcertificate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner districtSpinner;
+    ArrayAdapter<CharSequence> districtAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        districtSpinner = (Spinner)findViewById(R.id.district_spinner);
+        districtAdapter = ArrayAdapter.createFromResource(this, R.array.district_array, android.R.layout.simple_spinner_item);
+        districtAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        districtSpinner.setAdapter(districtAdapter);
+        districtSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position) + "", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
+
 }
